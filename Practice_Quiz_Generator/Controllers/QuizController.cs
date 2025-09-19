772e28f7e6a10ab49166197cce4808f4b9e8e116
Praise Aribisala <aribisalapraise12@gmail.com>
2025-09-19 17:16:51 +0100

    commit

--- Practice_Quiz_Generator/Controllers/QuizController.cs
+++ Practice_Quiz_Generator/Controllers/QuizController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Practice_Quiz_Generator.Application.Services;
 using Practice_Quiz_Generator.Domain.Models;
+using System.Security.Claims;
 
 namespace Practice_Quiz_Generator.Controllers
 {
@@ -11,10 +12,12 @@ namespace Practice_Quiz_Generator.Controllers
     public class QuizController : ControllerBase
     {
         private readonly QuizService _quizService;
+        private readonly QuizGenerationService _quizGenerationService;
 
-        public QuizController(QuizService quizService)
+        public QuizController(QuizService quizService, QuizGenerationService quizGenerationService)
         {
             _quizService = quizService;
+            _quizGenerationService = quizGenerationService;
         }
 
         [HttpGet("form-config")]
@@ -22,6 +25,28 @@ namespace Practice_Quiz_Generator.Controllers
         {
             var config = await _quizService.GetQuizFormConfigurationAsync(User);
             return Ok(config);
+        }
+
+        [HttpPost("generate")]
+        public async Task<IActionResult> GenerateQuiz([FromBody] QuizGenerationRequestDto request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+            var userId = int.Parse(userIdClaim?.Value ?? "0");
+            var (isValid, message) = await _quizGenerationService.ValidateRequestAsync(request, userId);
+
+            if (!isValid)
+            {
+                return BadRequest(message);
+            }
+
+            var quizRequestId = _quizGenerationService.EnqueueQuizGenerationRequest(request, userId);
+
+            return Accepted(new { quizRequestId = quizRequestId, message = "Quiz generation request has been accepted and is being processed." });
         }
     }
 }
--- Practice_Quiz_Generator/Program.cs
+++ Practice_Quiz_Generator/Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.ConfigureDatabase(builder.Configuration);
+builder.Services.AddScoped<QuizService>();
+builder.Services.AddScoped<QuizGenerationService>(); 
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
