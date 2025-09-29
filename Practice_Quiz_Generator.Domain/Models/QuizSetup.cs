7e9a7426d2a8e49c539666fdee54ae67ac3c2f7e
Aduragbemi Oduntan <aduragbemioduntan@gmail.com>
2025-09-29 07:09:32 +0100

    restructured quiz related entities

---
+++ Practice_Quiz_Generator.Domain/Models/QuizSetup.cs
@@ -0,0 +1,15 @@
+﻿namespace Practice_Quiz_Generator.Domain.Models
+{
+    public class QuizSetup : BaseEntity
+    {
+        public string QuestionType { get; set; } 
+        public int NumberOfQuestions { get; set; }
+        public string QuestionSource { get; set; } 
+        public int Timer { get; set; }
+        public string StudentId { get; set; }
+        public string CourseId { get; set; }
+
+        public User Student { get; set; }
+        public Course Course { get; set; }
+    }
+}
